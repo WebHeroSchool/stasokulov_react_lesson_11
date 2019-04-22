d783d6d095adcf73fd95f87e2ea8a2bc05d447b2
Stanislav Okulov <stasokulov@gmail.com>
2019-04-22 22:31:56 +0300

    Пулл реквест для задания 11 по React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './myClass.css';
+
+const foo = 'Строка. Применил к ней импортированный стиль';
+const num1 = 2;
+const num2 = 3;
+const num3 = undefined;
+let num4 = null;
+const style = 'myClass';
 
 class App extends Component {
   render() {
@@ -19,6 +27,38 @@ class App extends Component {
           >
             Learn React
           </a>
+          <div>
+            <p
+              className = {style}
+            >
+              {foo}
+            </p>
+            <p>
+              {num1}
+            </p>
+            <p>
+              {num1 + num2}
+            </p>
+            <p>
+              {num1&&'Логическая_операция'}
+            </p>
+            <p>
+              {num1>num2?'Тернарная операция 1':'Тернарная операция 2'}
+            </p>
+            <p>
+              {'Пытаюсь вывести undefined:'}{num3}
+            </p>
+            <p>
+              {'Пытаюсь вывести null:'}{num4}
+            </p>
+            <p>
+              {'Пытаюсь вывести false:'}{false&&true}
+            </p>
+            <p>
+              {'Пытаюсь вывести true:'}{false||true}
+            </p>
+            
+          </div>
         </header>
       </div>
     );
