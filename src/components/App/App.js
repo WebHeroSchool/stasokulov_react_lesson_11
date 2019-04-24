434e2059416d5067d60aff63dddf602faadc68da
Stanislav Okulov <stasokulov@gmail.com>
2019-04-24 08:45:30 +0300

    Задание 15, структура проекта

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import InputItem from '../InputItem/InputItem';
+import ItemsList from '../ItemsList/ItemsList.js';
+import Footer from '../Footer/Footer';
+
+const App = () => (
+  <div>
+    <h1>Важные дела:</h1>
+    <InputItem />
+    <ItemsList />
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Footer = () => (<span>Подвал</span>);
+
+export default Footer;
