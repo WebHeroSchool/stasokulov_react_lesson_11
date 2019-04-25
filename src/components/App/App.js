fc5c6730f334b3f48d3c67e38bf062f6cbabdc30
Stanislav Okulov <stasokulov@gmail.com>
2019-04-25 17:10:55 +0300

    Задание 24. Компоненты-классы

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,52 +7,40 @@ import Header from '../Header/Header';
 import InputItem from '../InputItem/InputItem';
 import ItemsList from '../ItemsList/ItemsList';
 import Footer from '../Footer/Footer';
-import myStyles from './App.module.css'; 
+import styles from './App.module.css'; 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
-
-const styles = theme => ({
-  root: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-});
-
-function PaperSheet(props) {
-  const { classes } = props;
-
-  const todos = [
-    {
-      value: 'Первое дело',
-      isDone: true
-    },
-    {
-      value: 'Второе дело',
-      isDone: false
-    },
-    {
-      value: 'Третье дело',
-      isDone: true
-    },
-  ];
-
-  return (
-    <div className={myStyles.wrap}>
-      <Paper className={classes.root} elevation={1}>
-        <Header />
-        <InputItem />
-        <ItemsList items={todos} />
-        <Footer count={3} />
-      </Paper>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const todos = [
+      {
+        value: 'Первое дело',
+        isDone: true
+      },
+      {
+        value: 'Второе дело',
+        isDone: false
+      },
+      {
+        value: 'Третье дело',
+        isDone: true
+      },
+    ];
+  
+    return (
+      <div className={styles.wrap}>
+        <Card>
+          <CardContent>
+            <Header />
+            <InputItem />
+            <ItemsList items={todos} />
+            <Footer count={3} />
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 }
 
-PaperSheet.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(PaperSheet);
+export default App;
