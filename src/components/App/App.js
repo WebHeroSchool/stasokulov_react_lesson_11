999c664602466b82cfaa44c665889398ee391b97
Stanislav Okulov <stasokulov@gmail.com>
2019-05-11 20:36:38 +0300

    Задание 35. React Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,126 +1,36 @@
 import React from 'react';
-import Header from '../Header/Header';
-import InputItem from '../InputItem/InputItem';
-import ItemsList from '../ItemsList/ItemsList';
-import Footer from '../Footer/Footer';
-import styles from './App.module.css'; 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+import Todo from '../Todo/Todo';
+import About from '../About/About';
+import Contacts from '../Contacts/Contacts';
+
+import styles from './App.module.css'; 
 
 class App extends React.Component {
-  state = {
-    todos: [
-      {
-        value: 'Первое дело',
-        isDone: false,
-        id: 1,
-      },
-      {
-        value: 'Второе дело',
-        isDone: false,
-        id: 2,
-      },
-      {
-        value: 'Третье дело',
-        isDone: false,
-        id: 3,
-      },
-    ],
-
-    input: {
-      value: '',
-      error: false,
-    },
-
-    count: 6,
-    numTask: 3,
-  };
-
-  onClickDone = id => {
-    const newItemList = this.state.todos.map(item => {
-      const newItem = {...item};
-      if (item.id === id) {
-        newItem.isDone = !item.isDone;
-      };
-      return newItem;
-    });
-    this.setState({todos: newItemList});
-  };
-
-  onClickDelete = id => {
-    const newItemList = this.state.todos.filter(item => {
-      return item.id !== id;
-    });
-    this.setState({todos: newItemList});
-  }
-
-  onChangeInput = event => {
-    const newInput = {...this.state.input};
-    newInput.value = event.target.value.toUpperCase();
-    this.setState({input: newInput});
-  }
-
-  addTask = () => {
-    //Проверяем не пуст ли инпут
-    if (this.state.input.value === '') {
-      const newInput = {...this.state.input};
-      newInput.label = 'Введите что-нибудь';
-      newInput.error = true;
-      this.setState({input: newInput});
-    } else {
-      //Создаем новый объект задачи
-      let newNumTask = this.state.numTask;
-      const newItem = {};
-      newItem.value = this.state.input.value;
-      newItem.isDone = false;
-      newItem.id = ++newNumTask;
-      //Клонируем массив задач, добавляем в конец новую задачу и переписываем массив в state
-      const newTodos = this.state.todos.slice();
-      newTodos.push(newItem);
-      this.setState({todos: newTodos});
-      //Обновляем инпут
-      const newInput = {...this.state.input};
-      newInput.value = '';
-      newInput.label = 'Добавить задачу';
-      newInput.error = false;
-      this.setState(
-        {
-          input: newInput,
-          numTask: newNumTask,
-        }
-      );
-    }
-  }
 
   render() {
     return (
-      <div className={styles.wrap}>
-        <Card>
-          <CardContent>
-            <Header />
-            <InputItem
-              value={this.state.input.value}
-              error={this.state.input.error}
-              onChange={this.onChangeInput}
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              className={styles.width100}
-              onClick={ () => this.addTask() }
-            >
-              Добавить
-            </Button>
-            <ItemsList
-              items={this.state.todos}
-              onClickDone={this.onClickDone}
-              onClickDelete={this.onClickDelete}
-            />
-            <Footer count={this.state.count} />
-          </CardContent>
-        </Card>
-      </div>
+      <Router>
+        <div className={styles.wrap}>
+          <Card className={styles.sidebar} >
+            <MenuList>
+              <Link className={styles.link} to='/'><MenuItem>Обо мне</MenuItem></Link>
+              <Link className={styles.link} to='/todo'><MenuItem>Дела</MenuItem></Link>
+              <Link className={styles.link} to='/contacts'><MenuItem>Контакты</MenuItem></Link>
+            </MenuList>
+          </Card>
+
+          <Card>
+            <Route path='/' exact component={About} />
+            <Route path='/todo' component={Todo} />
+            <Route path='/contacts' component={Contacts} />
+          </Card>
+        </div>
+      </Router>
     );
   }
 }
